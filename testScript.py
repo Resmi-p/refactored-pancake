d9bfd788f7a4e152c2162157b62300c627698d72
Scarfsman <george_warne@hotmail.co.uk>
2024-11-20 17:21:06 +0000

    Initial commit

---
+++ testScript.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+test = np.linspace(1, 10, 10)
+
+for i in test:
+    print(i)
+    
+    print('hello')
